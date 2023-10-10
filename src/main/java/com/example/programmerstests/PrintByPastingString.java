8642a96b9088ac5cc9ece0d110e32a0c778f7029
codky <msky0912@gmail.com>
2023-10-10 12:55:25 +0900

    2023-10-10 - PrintByPastingString - TwistTheString

---
+++ src/main/java/com/example/programmerstests/PrintByPastingString.java
@@ -0,0 +1,13 @@
+package com.example.programmerstests;
+
+import java.util.Scanner;
+
+public class PrintByPastingString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+
+        System.out.println(a+b);
+    }
+}
