f47657e232e72dc145d60019756a52efca37890c
msk <msky0912@gmail.com>
2023-10-18 08:15:30 +0900

    P1546_평균구하기

---
+++ src/main/java/com/example/programmerstests/P1546_평균.java
@@ -0,0 +1,27 @@
+package com.example.programmerstests;
+
+import java.util.Scanner;
+
+public class P1546_평균 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+//        int A[] = new int[N];
+//        for (int i=0; i<N; i++) {
+//            A[i] = sc.nextInt();
+//        }
+
+        long sum = 0;
+        long max = 0;
+
+        for (int i=0; i<N; i++) {
+//            if (max>A[i]) max = A[i];
+//            sum = sum + A[i];
+            int temp = sc.nextInt();
+            if (temp>max) max = temp;
+            sum = sum + temp;
+        }
+
+        System.out.println(sum*100.0/max/N);
+    }
+}
