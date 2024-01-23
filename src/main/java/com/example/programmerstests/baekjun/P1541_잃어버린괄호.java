21ca02f8ed224fdca0f158e884e28533bae05aa4
codky <msky0912@gmail.com>
2024-01-23 12:47:29 +0900

    P1541_잃어버린괄호

--- src/main/java/com/example/programmerstests/baekjun/P1541_잃어버린괄호.java
+++ src/main/java/com/example/programmerstests/baekjun/P1541_잃어버린괄호.java
@@ -4,23 +4,24 @@ import java.util.Scanner;
 
 public class P1541_잃어버린괄호 {
     static int answer = 0;
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String example = sc.nextLine();
-        String[] str = example.split("-");
-        for (int i=0; i<str.length; i++) {
-            int temp = mySum(str[i]);
+        Scanner scanner = new Scanner(System.in);
+        String example = scanner.nextLine();
+        String[] strings = example.split("-");
+        for (int i=0; i<strings.length; i++) {
+            int temp = mySum(strings[i]);
             if (i==0) answer = answer + temp;
             else answer = answer - temp;
         }
         System.out.println(answer);
     }
 
-    private static int mySum(String a) {
+    private static int mySum(String string) {
         int sum = 0;
-        String[] temp = a.split("[+]");
+        String[] temp = string.split("[+]");
 
-        for (int i = 0; i<temp.length; i++) {
+        for (int i=0; i< temp.length; i++) {
             sum = sum + Integer.parseInt(temp[i]);
         }
         return sum;
